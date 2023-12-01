05971225d185d126380e24cf6206d24eb6682612
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2023-12-01 20:03:00 +0100

    minor

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -8,5 +8,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  optimizeDeps: {
+    exclude: ["js-big-decimal"],
   },
 });
