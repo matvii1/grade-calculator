cdb5c395b3f1b1f32809ccc25a1a954eefc3882d
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2023-12-04 09:07:17 +0100

    calculated grade fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -21,7 +21,7 @@ function App() {
   }
 
   return (
-    <div className="h-[90dvh] bg-main bg-contain bg-bottom bg-no-repeat md:h-[95dvh] md:bg-contain">
+    <div className="h-[100dvh] bg-main bg-contain bg-bottom bg-no-repeat md:bg-60%">
       <div className="md:mt-18 mx-auto mt-2 max-w-md px-2 md:py-12">
         <div className="max-h-[98dvh] overflow-y-auto rounded-xl border-2 border-slate-400 bg-white/[0.5] px-8 py-4 backdrop-blur-md md:max-h-[620px]">
           <h1 className="mb-4 text-center text-2xl font-semibold">
